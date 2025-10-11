f4a224e6600f466e7bea71d2c7511a41bb9363d4
Jeffrey Graves <jagraves21@tntech.edu>
2025-10-10 23:24:22 -0500

    added variable names for files

--- src/ts_datasets/datasets/tourism.py
+++ src/ts_datasets/datasets/tourism.py
@@ -8,6 +8,16 @@ class Tourism(DatasetManager):
 	DATASET_URL = "https://robjhyndman.com/data/27-3-Athanasopoulos1.zip"
 
 	# --- Filenames ---
+	YEARLY_IN_FILE = "yearly_in.csv"
+	YEARLY_OOS_FILE = "yearly_oos.csv"
+	QUARTERLY_IN_FILE = "quarterly_in.csv"
+	QUARTERLY_OOS_FILE = "quarterly_oos.csv"
+	MONTHLY_IN_FILE = "monthly_in.csv"
+	MONTHLY_OOS_FILE = "monthly_oos.csv"
+
+	YEARLY_FILE = "yearly.csv"
+	QUARTERLY_FILE = "quarterly.csv"
+	MONTHLY_FILE = "monthly.csv"
 
 	def __init__(self, project_root=None):
 		super().__init__("Tourism", project_root=project_root)
@@ -39,63 +49,63 @@ class Tourism(DatasetManager):
 	# --- Raw Data Loaders ---
 	def load_yearly_in_data(self, verbose=True):
 		return self.load_raw_dataframe(
-			"yearly_in.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			YEARLY_IN_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 
 	def load_yearly_oos_data(self, verbose=True):
 		return self.load_raw_dataframe(
-			"yearly_oos.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			YEARLY_OOS_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 	
 	def load_quarterly_in_data(self, verbose=True):
 		return self.load_raw_dataframe(
-			"quarterly_in.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			QUARTERLY_IN_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 
 	def load_quarterly_oos_data(self, verbose=True):
 		return self.load_raw_dataframe(
-			"quarterly_oos.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			QUARTERLY_OOS_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 
 	def load_monthly_in_data(self, verbose=True):
 		return self.load_raw_dataframe(
-			"monthly_in.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			MONTHLY_IN_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 
 	def load_monthly_oos_data(self, verbose=True):
 		return self.load_raw_dataframe(
-			"monthly_oos.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			MONTHLY_OOS_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 
 	# -- Processed Data Loaders ---
 	def load_yearly_data(self, verbose=True):
 		return self.load_processed_dataframe(
-			"yearly.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			YEARLY_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 	
 	def load_quarterly_data(self, verbose=True):
 		return self.load_processed_dataframe(
-			"quarterly.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			QUARTERLY_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 
 	def load_monthly_data(self, verbose=True):
 		return self.load_processed_dataframe(
-			"monthly.csv", header=0, index_col=None, nrows=None, verbose=verbose
+			MONTHLY_FILE, header=0, index_col=None, nrows=None, verbose=verbose
 		)
 
 	# --- Processed Data Savers ---
 	def save_yearly_data(self, df, suppress_logs=True):
 		self.save_processed_dataframe(
-			df, "yearly.csv", append=False, index=False, suppress_logs=False
+			df, YEARLY_FILE, append=False, index=False, suppress_logs=False
 		)
 
 	def save_quarterly_data(self, df, suppress_logs=True):
 		self.save_processed_dataframe(
-			df, "quarterly.csv", append=False, index=False, suppress_logs=False
+			df, QUARTERLY_FILE, append=False, index=False, suppress_logs=False
 		)
 
 	def save_monthly_data(self, df, suppress_logs=True):
 		self.save_processed_dataframe(
-			df, "monthly.csv", append=False, index=False, suppress_logs=False
+			df, MONTHLY_FILE, append=False, index=False, suppress_logs=False
 		)
 
